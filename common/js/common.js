0312775588e2066a01b45916cdd71a8853e58400
yutakiku <yuta.kikuchi@photocreate.co.jp>
2018-01-26 18:00:04 +0900

    [lifestudio]base.html, common各ファイル更新

--- common/js/common.js
+++ common/js/common.js
@@ -1,16 +1,7 @@
 $(function () {
-	$('.sub-menu').on({
-        'mouseenter': function () {
-            $(this).addClass('is-active');
-        },
-        'mouseleave': function () {
-            $(this).removeClass('is-active');
-        }
-    });
-
-    $('#nav_toggle,#overlay').on('click', function() {
+    $('.sp_nav,.modal_overlay').on('click', function() {
         $('body').toggleClass('open');
     });
     var url = window.location;
-    $('.global-nav a[href="'+url+'"]').addClass('active');
+    $('nav a[href="'+url+'"]').addClass('current');
 });
