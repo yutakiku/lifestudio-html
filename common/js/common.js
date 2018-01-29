33c79d6591e3648cdcb5ad39d9cce422807e4b10
yutakiku <yuta.kikuchi@photocreate.co.jp>
2018-01-29 18:02:08 +0900

    [lifestudio]base.htmlなど更新

--- common/js/common.js
+++ common/js/common.js
@@ -2,6 +2,19 @@ $(function () {
     $('.sp_nav,.modal_overlay').on('click', function() {
         $('body').toggleClass('open');
     });
+
     var url = window.location;
     $('nav a[href="'+url+'"]').addClass('current');
+
+    $(window).on("scroll", function() {
+        scrollHeight = $(document).height(); //ドキュメントの高さ 
+        scrollPosition = $(window).height() + $(window).scrollTop(); //現在地 
+        footHeight = $("footer").innerHeight(); //footerの高さ（＝止めたい位置）
+        if ( scrollHeight - scrollPosition  <= footHeight ) { //ドキュメントの高さと現在地の差がfooterの高さ以下になったら
+            $(".sp_nav").addClass("is-hide"); //スマホメニューボタン非表示(.sp_navに.is-hide付与)
+        } else { //それ以外の場合は
+            $(".sp_nav").removeClass("is-hide"); //スマホメニューボタン表示(.sp_navから.is-hide削除)
+        }
+    });
+
 });
