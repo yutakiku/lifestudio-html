c5d27ea93f165ca4cb17ba5dfc3784475f52ea3a
yutakiku <yuta.kikuchi@photocreate.co.jp>
2018-02-07 18:14:48 +0900

    [lifestudio]reserveなど更新

--- common/js/common.js
+++ common/js/common.js
@@ -21,11 +21,24 @@ $(function () {
       startPos = currentPos; //スクロールの基準を揃える
     });
 
-    // アコーディオン
-    $('.accordion dd').hide(); //詳細非表示
-    $('.accordion dt').click(function(){ //dtクリックイベント
+    // アコーディオン 撮影内容選択
+    $('.reserve_details .accordion dd').hide(); //詳細非表示
+    $('.reserve_details .accordion dt').click(function(){ //dtクリックイベント
       $(this).next('dd').slideDown(); //クリックされたdtの次にあるddにslideDownを実行
       $(this).next('dd').siblings('dd').slideUp(); //クリックされたdtの次にあるdd以外のddにslideUpを実行
+    });
+
+    // アコーディオン キャンセル待ち通知登録
+    $('.waiting_list .accordion dl dd').hide(); //詳細非表示
+    $('.waiting_list .accordion dl dt').click(function(){ //dtクリックイベント
+      $(this).next('dd').slideToggle(); //クリックされたdtの次にあるddにslideToggleを実行
+      // 矢印箇所
+      if ($(this).hasClass('active')) { // activeが存在する場合
+        $(this).removeClass('active'); // activeを削除
+      }
+      else {
+        $(this).addClass('active'); // activeを追加
+      }
     });
 
 });
